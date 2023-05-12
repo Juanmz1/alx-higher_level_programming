07ba788500dafcc6509f11e5b126beb64caa133d
juanmz1 <juanmzii48@gmail.com>
2023-05-11 20:34:20 -0700

    task

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,11 +2,11 @@
 if __name__ == "__main__":
     from sys import argv
     length = len(argv)
-    if argv == 0:
-        print("0 argument :")
-    elif argv == 1:
-        print("1 argument :")
-    else:
-        print("{} arguments:".format(length))
+    if length == 0:
+        a = '.'
+        print("{} argument{}".format(length, a))
+    elif argv => 1:
+        a = 's:'
+        print("{} argument{}".format(length, a))
     for i in range(length):
         print("{} : {}".format(i + 1, argv[i + 1]))
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    length = len(argv) - 1
     add = 0
-    for i in range(length):
-        add += int(argv[i])
+    for i in argv[1:]:
+        add += int(i)
     print("{:d}2.format(add))
