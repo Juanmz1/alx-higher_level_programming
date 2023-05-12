e2b3858bcfaafed8b9c08bb14ac134a6ff5f1e5c
juanmz1 <juanmzii48@gmail.com>
2023-05-11 19:29:00 -0700

    task

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,23 +1,25 @@
 ##!/usr/bin/python3
-from sys import argv, exit
-from calculator_1 import add, sub, mul, div
-if argv != 3:
-    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    exit(1)
-operat = "+-*/"
-if operat not in argv[2]:
-    print("Unknown operator. Available operators: +, -, * and /")
-    exit(1)
-for i in operat:
-    if argv[2] == i:
-        sign = i
-a = int(argv[1])
-b = int(argv[3])
-if sign == '+':
-    print(f"{a} + {b} = {add(a, b)}")
-elif sign == '-':
-    print(f"{a} - {b} = {sub(a, b)}")
-elif sign == '*':
-    print(f"{a} * {b} = {mul(a, b)}")
-elif sign == '/':
-    print(f"{a} / {b} = {div(a, b)}")
+if __name__ == "__main__":
+    from sys import argv, exit
+    from calculator_1 import add, sub, mul, div
+    length_1 = len(argv) - 1
+    if length_1 != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    operat = "+-*/"
+    if argv[2] not in operat:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    for i in operat:
+        if argv[2] == i:
+            sign = i
+    a = int(argv[1])
+    b = int(argv[3])
+    if sign == '+':
+        print(f"{a} + {b} = {add(a, b)}")
+    elif sign == '-':
+        print(f"{a} - {b} = {sub(a, b)}")
+    elif sign == '*':
+        print(f"{a} * {b} = {mul(a, b)}")
+    elif sign == '/':
+        print(f"{a} / {b} = {div(a, b)}")
