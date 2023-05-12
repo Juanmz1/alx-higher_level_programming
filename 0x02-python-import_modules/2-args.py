5180d428fea5ea63bef921383d99768c912d8ba4
juanmz1 <juanmzii48@gmail.com>
2023-05-12 04:06:37 -0700

    task

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from sys import argv
-    length = len(argv) - 1
-    c = ':'
-    if length == 0:
-        c = 's.'
-    if length => 1:
-        c = 's:'
-    print("{} argument{}".format(length, c))
-    for i in range(1, length + 1):
-        print("{} : {}".format(i, argv[i]))
+import sys
+length = len(sys.argv) - 1
+if length == 0:
+    print("0 arguments.")
+elif length == 1:
+    print("1 argument:")
+else:
+    print("{} arguments:".format(length))
+for i in range(length):
+     print("{}: {}".format(i + 1, sys.argv[i + 1]))
