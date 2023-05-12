241c9bb510f4d1e75e167708dd693585d8613847
juanmz1 <juanmzii48@gmail.com>
2023-05-11 19:33:47 -0700

    task

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,11 +3,11 @@ if __name__ == "__main__":
     from sys import argv, exit
     from calculator_1 import add, sub, mul, div
     length_1 = len(argv) - 1
+    ope = "+-*/"
     if length_1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    operat = "+-*/"
-    if argv[2] not in operat:
+    if argv[2] not in ope:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
     for i in operat:
