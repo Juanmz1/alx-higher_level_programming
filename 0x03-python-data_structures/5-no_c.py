4aaba20b9bb03438a99f81cf9d8d8498470c089d
juanmz1 <juanmzii48@gmail.com>
2023-05-14 20:25:19 -0700

    task

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    my_string_new = my_string.translate({ord("c"): None for i in "cC"})
+    my_string_new = my_string.translate({ord('c'): None})
+    my_string_new = my_string_new.translate({ord('C'): None})
     return my_string_new
