dda30c0433f03d4f0f80e59df5a945bca81e1eb8
juanmz1 <juanmzii48@gmail.com>
2023-05-14 20:45:29 -0700

    task

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,6 +2,6 @@
 def multiple_returns(sentence):
     charater_1 = sentence[0]
     length_sen = len(sentence)
-    if length_sen > 0:
-        return (length_sen, charater_1)
-    return (length_sen, None)
+    if length_sen < 0:
+        return (charater_1, None)
+    return (length_sen, charater_1)
