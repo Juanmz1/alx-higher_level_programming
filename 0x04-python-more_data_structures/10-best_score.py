6a0b6ab74287a6b1143657816bd2c22ff92be1f5
juanmz1 <juanmzii48@gmail.com>
2023-05-18 09:09:10 -0700

    task

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if len(a_dictionary) == 0:
-        return None
-    for i in a_dictionary.item():
-        i.sort(key = lambda x: x[i])
-    for i in sorted a_dictionary.item():
-    return i[-1][0]
+    if type(a_dictionary) == dict:
+        if len(a_dictionary) == 0:
+            return None
+        a = list(a_dictionary.items())
+        a.sort(key=lambda n: n[1])
+        return a[-1][0]
     return None
