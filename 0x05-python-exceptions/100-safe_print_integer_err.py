b9dbb4109162969bb2314170b927baff8b147d2a
juanmz1 <juanmzii48@gmail.com>
2023-05-29 15:25:09 -0700

    task 5

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,6 +4,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except Exception as e:
+    except (ValueError, TypeError) as e:
         print("Exception: {}", e, file=stderr)
         return False
