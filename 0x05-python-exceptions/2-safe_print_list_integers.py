a9df326a1d5cacf07f49e08b531e1d3ee220556c
juanmz1 <juanmzii48@gmail.com>
2023-05-29 14:42:21 -0700

    task

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -9,6 +9,5 @@ def safe_print_list_integers(my_list=[], x=0):
             num += 1
     except IndexError:
         pass
-    finally:
-        print()
-        return num
+    print()
+    return num
