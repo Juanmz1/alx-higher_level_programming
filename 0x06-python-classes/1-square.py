cfedf5d26473ee824536b75520ea42d4fc47cf4e
juanmz1 <juanmzii48@gmail.com>
2023-05-31 15:31:24 -0700

    task

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -7,14 +7,10 @@ class Square:
     Attributes:
     __size(int): size of the square
     """
-
-
     def __init__(self, size):
         """ initialize a square
         Args:
         __size(int):size of the square
         Returns: None
         """
-
-
         self.__size = size
--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,16 +7,12 @@ class Square:
     Attributes:
     __size(int): size of the square
     """
-
-
     def __init__(self, size = 0):
         """ initialize a square
         Args:
         __size(int):size of the square
         Returns: None
         """
-
-
         self.__size = size
         if type(size) is not int:
             raise TypeError(size must be an integer)
